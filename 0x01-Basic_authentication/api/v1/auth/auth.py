26991a7c546d6faf4713c1107f9c19b074b248ae
ahmedhelalley <a.helalley@gmail.com>
2024-09-04 03:12:28 +0300

    complete

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Authentications module"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Implement Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require auth"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if not path.endswith('/'):
+            path = path + '/'
+
+        for excluded_path in excluded_paths:
+            if (excluded_path.endswith('*')
+                    and path.startswith(excluded_path[:-1])):
+                return False
+
+            if path == excluded_path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """Authrization header"""
+        if request is None:
+            return None
+
+        header = request.headers.get('Authorization')
+        if not header:
+            return None
+
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Current user"""
+        return None
